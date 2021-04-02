ea834efe49c70591b38de1718bc11841c6ed47bc
tatyana-star <80785523+tatyana-star@users.noreply.github.com>
2021-04-02 16:02:31 -0400

    Update selectors.js

--- data/selectors.js
+++ data/selectors.js
@@ -6,12 +6,20 @@ const elements = {
     "radioButtons": ".ant-radio",
     "radioButtonsLabel": ".ant-radio-wrapper",
     "age": "#age",
+    "storyLabel": "Label[for=story]",
     "storyType": ".ant-select-selector",
     "storyList": ".ant-select-item",
+    "storyPH": ".ant-select-selection-placeholder",
     "imageLabel": ".ant-form-item-label",
     "image": "span.ant-upload",
     "submit": ".ant-btn",
     "tryAgain": ".btn-primary",
+    "tryAgainBtn": ".btn-primary",
+    "error": "div[role=alert]",
+    "errorAge": ".ant-form-item-explain",
+    "outside": "#root",
+    "imageThumbnail": ".ant-upload-list-item-thumbnail",
+    "binIcon": ".ant-upload-list-item-actions",
 
 }
 module.exports = elements;
